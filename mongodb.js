c7e6d359ea9fc5f05f92c7e7655917711a959038
Priyansh Neel <neelpriyansh@gmail.com>
2025-04-22 14:48:44 +0530

    PORT commit

--- mongodb.js
+++ mongodb.js
@@ -1,10 +1,10 @@
 import mongoose, { mongo } from 'mongoose';
 
-const URI = "mongodb://localhost:27017/dbmserver"  
+const MONGO_URI = "mongodb+srv://neelpriyansh:BUHM0hbEryFmL4Aw@cluster0.mtjrnw1.mongodb.net/";
 
 const mongodb = async() => {
     try {
-        await mongoose.connect(URI)
+        await mongoose.connect(MONGO_URI)
         console.log("Connection Succcess");
     } catch (error) {
         console.error("Database Failed") 
