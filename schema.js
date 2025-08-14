4c47b52b0d9b24cf9eace7be709d97bd1c3476d0
Priyansh Neel <neelpriyansh@gmail.com>
2025-08-14 11:54:25 +0530

    Backend Contri

--- schema.js
+++ schema.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const userschema = new mongoose.Schema({
   username: { type: String, required: true },
-  password: { type: String, required: true },
+  password: { type: String, required: true },  
   discriptor: [Number] // 128-length float array
 });
 
